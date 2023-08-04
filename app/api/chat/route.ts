f3b2c8f2a3beb3d5d0be3929b2c682d0f16e1877
Alper Değre <alper.degre.dev@gmail.com>
2023-08-04 20:20:41 +0300

    change api routes

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,16 +1,11 @@
 import { kv } from "@vercel/kv";
 import { Ratelimit } from "@upstash/ratelimit";
-import { Configuration, OpenAIApi } from "openai-edge";
-import { OpenAIStream, StreamingTextResponse } from "ai";
-import { functions, runFunction } from "./functions";
-
-// Create an OpenAI API client (that's edge friendly!)
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(config);
-
-export const runtime = "edge";
+import { Message, StreamingTextResponse } from "ai";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { PineconeClient } from "@pinecone-database/pinecone";
+import { createChain } from "@/libs/chain";
+import { AIMessage, HumanMessage } from "langchain/schema";
 
 export async function POST(req: Request) {
   if (
@@ -29,7 +24,7 @@ export async function POST(req: Request) {
     );
 
     if (!success) {
-      return new Response("You have reached your request limit for the day.", {
+      return new Response("Bugünkü istek sınırına ulaştınız.", {
         status: 429,
         headers: {
           "X-RateLimit-Limit": limit.toString(),
@@ -40,45 +35,49 @@ export async function POST(req: Request) {
     }
   }
 
-  const { messages } = await req.json();
+  try {
+    const { messages } = await req.json();
 
-  // check if the conversation requires a function call to be made
-  const initialResponse = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo-0613",
-    messages,
-    functions,
-    function_call: "auto",
-  });
-  const initialResponseJson = await initialResponse.json();
-  const initialResponseMessage = initialResponseJson?.choices?.[0]?.message;
+    const pinecone = new PineconeClient();
 
-  let finalResponse;
-
-  if (initialResponseMessage.function_call) {
-    const { name, arguments: args } = initialResponseMessage.function_call;
-    const functionResponse = await runFunction(name, JSON.parse(args));
-
-    finalResponse = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo-0613",
-      stream: true,
-      messages: [
-        ...messages,
-        initialResponseMessage,
-        {
-          role: "function",
-          name: initialResponseMessage.function_call.name,
-          content: JSON.stringify(functionResponse),
-        },
-      ],
+    await pinecone.init({
+      environment: process.env.PINECONE_ENVIRONMENT ?? "",
+      apiKey: process.env.PINECONE_API_KEY ?? "",
     });
-    // Convert the response into a friendly text-stream
-    const stream = OpenAIStream(finalResponse);
-    // Respond with the stream
-    return new StreamingTextResponse(stream);
-  } else {
-    // if there's no function call, just return the initial response
-    // but first, we gotta convert initialResponse into a stream with ReadableStream
-    const chunks = initialResponseMessage.content.split(" ");
+
+    const index = pinecone.Index(process.env.PINECONE_INDEX ?? "");
+
+    // Create vectorstore
+    const vectorStore = await PineconeStore.fromExistingIndex(
+      new OpenAIEmbeddings({}),
+      {
+        pineconeIndex: index,
+      },
+    );
+
+    const result = await vectorStore.similaritySearch("muhtaç olduğun kudret");
+    console.log(result);
+    
+    const history = messages.map((message: Message) => {
+      return message.role === "user"
+        ? new HumanMessage(message.content)
+        : new AIMessage(message.content);
+    });
+
+    const question = messages[messages.length - 1].content;
+
+    // Create chain
+    const chain = createChain(vectorStore);
+
+    // Ask a question using chat history
+    const response = await chain.call({
+      question: question,
+      chat_history: history || [],
+    });
+
+    console.log(response);
+    
+    const chunks = response.text.split(" ");
     const stream = new ReadableStream({
       async start(controller) {
         for (const chunk of chunks) {
@@ -96,5 +95,62 @@ export async function POST(req: Request) {
       },
     });
     return new StreamingTextResponse(stream);
+  } catch (e) {
+    console.log(e);
   }
+  // check if the conversation requires a function call to be made
+  // const initialResponse = await openai.createChatCompletion({
+  //   model: "gpt-3.5-turbo-0613",
+  //   messages,
+  //   functions,
+  //   function_call: "auto",
+  // });
+  // const initialResponseJson = await initialResponse.json();
+  // const initialResponseMessage = initialResponseJson?.choices?.[0]?.message;
+
+  // let finalResponse;
+
+  // if (initialResponseMessage.function_call) {
+  //   const { name, arguments: args } = initialResponseMessage.function_call;
+  //   const functionResponse = await runFunction(name, JSON.parse(args));
+
+  //   finalResponse = await openai.createChatCompletion({
+  //     model: "gpt-3.5-turbo-0613",
+  //     stream: true,
+  //     messages: [
+  //       ...messages,
+  //       initialResponseMessage,
+  //       {
+  //         role: "function",
+  //         name: initialResponseMessage.function_call.name,
+  //         content: JSON.stringify(functionResponse),
+  //       },
+  //     ],
+  //   });
+  //   // Convert the response into a friendly text-stream
+  //   const stream = OpenAIStream(finalResponse);
+  //   // Respond with the stream
+  //   return new StreamingTextResponse(stream);
+  // } else {
+  //   // if there's no function call, just return the initial response
+  //   // but first, we gotta convert initialResponse into a stream with ReadableStream
+  //   const chunks = initialResponseMessage.content.split(" ");
+  //   const stream = new ReadableStream({
+  //     async start(controller) {
+  //       for (const chunk of chunks) {
+  //         const bytes = new TextEncoder().encode(chunk + " ");
+  //         controller.enqueue(bytes);
+  //         await new Promise((r) =>
+  //           setTimeout(
+  //             r,
+  //             // get a random number between 10ms and 30ms to simulate a random delay
+  //             Math.floor(Math.random() * 20 + 10),
+  //           ),
+  //         );
+  //       }
+  //       controller.close();
+  //     },
+  //   });
+  //   return new StreamingTextResponse(stream);
+  // }
 }
