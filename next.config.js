6dae87474af5c74b48f19d3803c34e15f541b459
Alper Değre <alper.degre.dev@gmail.com>
2023-08-09 19:58:19 +0300

    config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     serverActions: true,
   },
-  assetPrefix: "https://nutuk.vercel.app/",
+  assetPrefix: isProd ? "https://nutuk.vercel.app/" : undefined,
 };
 
 module.exports = nextConfig;
